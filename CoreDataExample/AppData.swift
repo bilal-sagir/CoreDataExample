640c2a44c6788e45c30e8be4ef48a2d261dbaa29
bilal-sagir <sagir.bilal@icloud.com>
2021-11-28 19:08:46 +0300

    coredata example

---
+++ CoreDataExample/AppData.swift
@@ -0,0 +1,18 @@
+import Foundation
+import CoreData
+
+
+class AppData{
+    private init (){ }
+    static let shared = AppData()
+    
+    var container: NSPersistentContainer = {
+        let c = NSPersistentContainer(name: "CoreDataExample")
+        c.loadPersistentStores { (description, err) in
+            if let error = err as NSError? { }
+        }
+        return c
+    }()
+
+    
+}
