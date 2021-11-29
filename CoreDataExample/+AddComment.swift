2f73784f6223ee45b28e2867f7a79cfe33729a93
bilal-sagir <sagir.bilal@icloud.com>
2021-11-29 03:58:56 +0300

    get comments

--- CoreDataExample/+AddComment.swift
+++ CoreDataExample/+AddComment.swift
@@ -2,6 +2,16 @@ import Foundation
 
 extension ViewController
 {
-    @IBAction func addComment(_ sender: Any) {
+    @IBAction func addComment(_ sender: Any)
+    {
+        let comment = Comment(context: context)
+        comment.body = "My comment"
+        comment.title = "Great"
+        
+        comment.setValue(Date(), forKey: "date")
+        
+        self.curUser.addToComments(comment)
+        
+        AppData.shared.saveContext()
     }
 }
--- CoreDataExample/+GetComments.swift
+++ CoreDataExample/+GetComments.swift
@@ -1,8 +1,25 @@
 import Foundation
+import CoreData
 
 extension ViewController
 {
-    @IBAction func getComments(_ sender: Any) {
+    @IBAction func getComments(_ sender: Any)
+    {
+        let request = NSFetchRequest <NSFetchRequestResult> (entityName: "Comment")
+        
+        
+        do{
+        let res = try context.fetch(request)
+            for data in res as! [NSManagedObject]{
+                let fetchComment = data as! Comment
+                
+                print("these are comments")
+                
+                print(fetchComment.title)
+            }
+            print("we found: \(res)")
+            
+        }catch{ }
     }
 }
 
