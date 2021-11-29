561af22dec84e8e9fdfd238596499ad52f1fedc2
bilal-sagir <sagir.bilal@icloud.com>
2021-11-29 03:48:52 +0300

    fetch user

--- CoreDataExample/+FetchUser.swift
+++ CoreDataExample/+FetchUser.swift
@@ -1,7 +1,20 @@
 import Foundation
+import CoreData
 
 extension ViewController
 {
-    @IBAction func fetchUser(_ sender: Any) {
+    @IBAction func fetchUser(_ sender: Any)
+    {
+        let request = NSFetchRequest <NSFetchRequestResult> (entityName: "User")
+        
+        let predicate = NSPredicate(format: "name == %@", "Adam")
+        
+        request.predicate = predicate
+        
+        do{
+        let res = try context.fetch(request)
+            print("we found: \(res)")
+            
+        }catch{ }
     }
 }
