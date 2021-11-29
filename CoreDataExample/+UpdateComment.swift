282555f3cdba42a74f41fc2165b2b78c72f70c95
bilal-sagir <sagir.bilal@icloud.com>
2021-11-29 04:25:54 +0300

    edit stuff

--- CoreDataExample/+UpdateComment.swift
+++ CoreDataExample/+UpdateComment.swift
@@ -1,7 +1,27 @@
 import Foundation
+import CoreData
 
 extension ViewController
 {
-    @IBAction func updateComment(_ sender: Any) {
+    @IBAction func updateComment(_ sender: Any)
+    {
+        let req = NSFetchRequest <NSFetchRequestResult>(entityName: "User")
+        
+        req.predicate = NSPredicate(format: "name == %@", "John")
+        
+        do{
+            let res = try context.fetch(req)
+            
+            for data in res as! [NSManagedObject]{
+                let usr = data  as! User
+                
+                usr.name = "A very different name"
+                AppData.shared.saveContext()
+            }
+            
+        }catch{
+            
+        }
+        
     }
 }
