7e4fefbffdb13bcfa1567c13a91a72c6d122e403
bilal-sagir <sagir.bilal@icloud.com>
2021-11-29 03:43:40 +0300

    save user

--- CoreDataExample/+SaveUser.swift
+++ CoreDataExample/+SaveUser.swift
@@ -4,5 +4,19 @@ extension ViewController
 {
     
     @IBAction func saveUser(_ sender: Any) {
+        
+        curUser = User(context: context)
+        
+        curUser.setValue("Adam", forKey: "name")
+        
+        let userAvatar = Image (context: context)
+        userAvatar.name = "myImg.jpeg"
+        userAvatar.setValue(Date(),
+                            forKey: "date")
+        
+        curUser.image = userAvatar
+        
+        AppData.shared.saveContext()
+        
     }
 }
